3712827d441746e9ed05be532951aa7636bb86ac
johnmberger <johnmartinberger@gmail.com>
2016-11-04 09:28:23 -0600

    moved files to src folder and got textblob running

--- src/router.py
+++ src/router.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 import test
+import bot
 
 app = Flask(__name__)
+
 fromAnother = test.other()
+blob = bot.getBlob()
 
 @app.route('/')
 def hello_world():
     return jsonify({
-    'Message': fromAnother,
-    'Other Message': 'Hi'
+        'Message': fromAnother,
+        'Other Message': 'Hi',
+        'TextBlob': blob
     })
