97dd7c7fa9fa5b99622fe6080aadbb2eb99daa7d
johnmberger <johnmartinberger@gmail.com>
2016-11-07 10:12:01 -0700

    updated router.py

--- src/router.py
+++ src/router.py
@@ -27,3 +27,8 @@ def get_comments(redditUsername):
     if sentence == 'null':
         sentence = markov.makeSentence(comments)
     return jsonify({"sentence": sentence})
+
+ if __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
